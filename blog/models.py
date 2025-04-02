c9b8dd4533b2412cf69a749c24dc3d4c72aa9972
jpca92 <jpca92@gmail.com>
2025-04-01 22:05:20 -0500

    lesson 4_2

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Articulo(models.Model):
+    titulo = models.Charfield(max_length=200)
+    imagen = models.Charfield(max_length=255)
+    autor = models.CharField(max_length=150)

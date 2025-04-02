34bf5672daed2d618238f156a3e4a3a759146bb2
jpca92 <jpca92@gmail.com>
2025-04-01 22:49:18 -0500

    lesson 4_5

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Articulo(models.Model):
     titulo = models.CharField(max_length=200)
     imagen = models.CharField(max_length=255)
-    autor = models.CharField(max_length=150)
+    # autor = models.CharField(max_length=150)
+    autor = models.ForeignKey(User, on_delete=models.RESTRICT)
+
 
     def __str__(self):
         return self.titulo

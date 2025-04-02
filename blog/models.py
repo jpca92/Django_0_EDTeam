bcc6781cdf6407d27c236160eeabeb8cfd436c48
jpca92 <jpca92@gmail.com>
2025-04-01 22:37:04 -0500

    lesson 4_4

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ class Articulo(models.Model):
     titulo = models.CharField(max_length=200)
     imagen = models.CharField(max_length=255)
     autor = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.titulo
